e9788a8cff05f8ebcd8d7262444bd1183cdf9393
Dark Tornado <dt3141592@naver.com>
2021-11-12 23:51:24 +0900

    일단 글자 긁어옴

--- index.py
+++ index.py
@@ -7,3 +7,10 @@ html = BeautifulSoup(response.text, 'html.parser')
 
 title = html.select_one('div.se-module.se-module-text.se-title-text').get_text().strip()
 print(title)
+
+data = html.select_one('div.se-main-container').select('span')
+result = []
+for datum in data:
+    datum = datum.get_text()
+    if datum != '\u200b': result.append(datum)
+print(result)
